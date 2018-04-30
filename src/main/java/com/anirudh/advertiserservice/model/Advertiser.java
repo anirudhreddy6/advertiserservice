0fc62dd87cac2aedf5da1f4b9802fc78bdf986c0
= <anirudhreddy06.com>
2018-04-29 23:07:00 -0500

    Removed Commented code in model

--- src/main/java/com/anirudh/advertiserservice/model/Advertiser.java
+++ src/main/java/com/anirudh/advertiserservice/model/Advertiser.java
@@ -1,60 +1,17 @@
 package com.anirudh.advertiserservice.model;
 
-
 import lombok.*;
-
-import javax.validation.constraints.NotNull;
-
 
 @Getter@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Advertiser {
 
-    @NotNull
+
     private String advertiserName;
-    @NotNull
     private String contactName;
     private double creditLimit;
 
-/*
-    public Advertiser(String advertiserName, String contactName, double creditLimit) {
-        this.advertiserName = advertiserName;
-        this.contactName = contactName;
-        this.creditLimit = creditLimit;
-    }
-    public Advertiser()
-    {
-
-    }
-
-
-
-    public String getAdvertiserName() {
-        return advertiserName;
-    }
-    public void setAdvertiserName(String advertiserName) {
-        this.advertiserName = advertiserName;
-    }
-
-    public String getContactName() {
-        return contactName;
-    }
-
-    public void setContactName(String contactName) {
-        this.contactName = contactName;
-    }
-
-    public double getCreditLimit() {
-        return creditLimit;
-    }
-
-    public void setCreditLimit(double creditLimit) {
-        this.creditLimit = creditLimit;
-    }
-
-
-    */
 
 
 
